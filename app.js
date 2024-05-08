6d6edd8b5391308a1995b8d2dbcffcf491f97250
benjaminthurston <benjamin.thurston87@gmail.com>
2024-05-07 23:48:16 -0400

    Correcting error
    I had functions that were not needed I am taking them out and building the correct ones.

--- app.js
+++ app.js
@@ -43,20 +43,8 @@ const randomBtn = document.querySelector('.random-btn');
 let currentItem = 0;
 
 //load intial item
-window.addEventListener('DOMContentLoaded',function(){
-  console.log("shake and bake");
-});
+window.addEventListener('DOMContentLoaded',function(){});
 
-//55:02
-
-// load initial item
-window.addEventListener('DOMContentLoaded', function () {
-  const item = reviews[currentItem];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
-});
 
 // show person based on item
 function showPerson(person) {
@@ -66,26 +54,3 @@ function showPerson(person) {
   job.textContent = item.job;
   info.textContent = item.text;
 }
-// show next person
-nextBtn.addEventListener('click', function () {
-  currentItem++;
-  if (currentItem > reviews.length - 1) {
-    currentItem = 0;
-  }
-  showPerson(currentItem);
-});
-// show prev person
-prevBtn.addEventListener('click', function () {
-  currentItem--;
-  if (currentItem < 0) {
-    currentItem = reviews.length - 1;
-  }
-  showPerson(currentItem);
-});
-// show random person
-randomBtn.addEventListener('click', function () {
-  console.log('hello');
-
-  currentItem = Math.floor(Math.random() * reviews.length);
-  showPerson(currentItem);
-});
